43707d364c7fce7cebdf8984543222613e51d1cf
vikram <vikramnsit@gmail.com>
2025-01-09 09:51:14 -0500

    First commit

--- projects/etls/api/weathermap.py
+++ projects/etls/api/weathermap.py
@@ -1,31 +1,22 @@
-
-from sqlalchemy import text, create_engine
-import pyodbc
 from projects.config import Config
-import pandas as pd
-import sqlalchemy as sa
+import projects.db
+import requests
 
-content = Config.load_config(Config)
+class Weather:
 
-SERVER = content["database"]["server"]
-DATABASE = content["database"]["db"]
+    def __init__(self):
+        super().__init__()
+        content = Config.load_config()["weathermap"]
+        self.apikey = content['apikey']
+        self.part = content['part']
+        self.lon = content['longitude']
+        self.lat = content['latitude']
+        self.api_url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apikey}'
 
-connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes;'
-# connectionString1 = f"mssql+pyodbc://@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
-#
-conn = pyodbc.connect(connectionString)
-print("Connection successful!")
-cursor = conn.cursor()
-SQL = "SELECT * FROM dbo.weather"
+    def extract(self):
+        print(self.api_url)
+        requests.request()
 
-cursor.execute(SQL)
-rows = cursor.fetchall()
 
-# print(df)
-#
-# try:
-#     with engine.connect() as connection:
-#         result = connection.execute(text("SELECT 1"))
-#         print(f"Connected successfully to {SERVER}");
-# except Exception as e:
-#     print("Connection failed:", e)
+w = Weather()
+w.extract()
