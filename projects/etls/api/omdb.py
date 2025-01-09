b3cea4f7af156e2069e52ec34a29f05690d31b76
vikram <vikramnsit@gmail.com>
2025-01-09 14:59:14 -0500

    imdb ETL commit

---
+++ projects/etls/api/omdb.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import json
+from projects.config import Config
+import projects.db as db
+import requests
+
+
+class Movies:
+
+    def __init__(self):
+        super().__init__()
+        content = Config.load_config()["omdb"]
+        self.apikey = content['apikey']
+        self.api_url = f'http://www.omdbapi.com/?i=tt0111161&apikey={self.apikey}&'
+
+    def extract(self):
+        print(self.api_url)
+        headers = {
+            "Api-Key": {self.apikey},
+            "Accept": "application/json"
+        }
+
+        response = requests.get(self.api_url)
+
+        if response.status_code == 200:
+            api_content = response.json()
+            df = pd.DataFrame(api_content)
+            columns_list = df.columns
+            print(columns_list)
+            df['Ratings'] = df['Ratings'].apply(lambda x: json.dumps(x) if isinstance(x, dict) else x)
+
+            return df
+        else:
+            status = response.status_code
+            print(f'Bad request: {status}')
+
+    def load(self, df:pd.DataFrame):
+        engine = db.get_db_engine()
+        df.to_sql("Movies", engine, index=False, if_exists="append")
+
+w = Movies()
+df = w.extract()
+w.load(df)
--- projects/etls/api/weathermap.py
+++ projects/etls/api/weathermap.py
@@ -2,6 +2,7 @@ from projects.config import Config
 import projects.db
 import requests
 
+
 class Weather:
 
     def __init__(self):
@@ -11,11 +12,23 @@ class Weather:
         self.part = content['part']
         self.lon = content['longitude']
         self.lat = content['latitude']
-        self.api_url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apikey}'
+        self.api_url = f'https://api.openweathermap.org/data/3.0/onecall?lat={self.lat}&lon={self.lon}&exclude={self.part}&appid={self.apikey}'
 
     def extract(self):
         print(self.api_url)
-        requests.request()
+        headers = {
+            "Api-Key": {self.apikey},
+            "Accept": "application/json"
+        }
+
+        response = requests.get(self.api_url, headers=headers)
+
+        if response.status_code == 200:
+            api_content = response.json()
+            print(api_content)
+        else:
+            status = response.status_code
+            print(f'Bad request: {status}')
 
 
 w = Weather()
